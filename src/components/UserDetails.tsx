246c90e80e0b3fa8c678382786cbed0ea9ebaf53
marina eleshaa <marinaeleshaa>
2025-02-20 00:58:21 +0200

    n

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -6,7 +6,7 @@ import { IUserData } from "../interfaces"
  
  const UserDetails =({user}:IProps)=>{
     return (
-        <div style={{margin:30}}>
+        <div style={{margin:20}}>
             <h3>username:{user.userName}</h3>
             <h3>email:{user.email}</h3>
             <h3>address:{user.address}</h3>
